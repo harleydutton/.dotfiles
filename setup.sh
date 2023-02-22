e670105a7bf3ed8b4a97c4dab409371af83a6f1e
harleydutton <harleydutton@gmail.com>
2023-02-21 20:50:47 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 cp .vimrc ~/.vimrc
 cp .inputrc ~/.inputrc
 cp .zshrc ~/.zshrc
-export MY_PACKAGES="vim zsh tmux"
-export MY_APPS="discord obsidian"
+#cli=vim zsh tmux
+#apps=discord obsidian chrome
+#extensions=bitwarden
