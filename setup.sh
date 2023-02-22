1d1d9176eb962f813cdb9acff739e075a34ced1e
Dutton <harley.dutton@charter.com>
2023-02-22 15:39:35 -0700

    figuring out a setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,18 @@
 cp .vimrc ~/.vimrc
 cp .inputrc ~/.inputrc
 cp .zshrc ~/.zshrc
+
+#determine package manager
+PM="something or other"
+
+
 #cli=vim zsh tmux
 #apps=discord obsidian chrome
 #extensions=bitwarden
+#languages=java17
+
+#install commentary for vim
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/commentary.git
+vim -u NONE -c "helptags commentary/doc" -c q
---
+++ temp.sh
@@ -0,0 +1,35 @@
+#define what this runs as i.e. !/bin/bash 
+echo "input package manager install command i.e. apt-get install"
+read PMI
+myPacks="jq"
+$PMI "$myPacks"
+
+PLATFORM="$(python3 -m platform)"
+echo $PLATFORM
+
+SUB='Mac'
+if [[ "$PLATFORM" =~ *"$SUB"* ]]; then
+  echo "It's there."
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
