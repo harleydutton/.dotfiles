93e6f446d2993beceeca9024e5aa49ef677eaf63
harleydutton <harleydutton@gmail.com>
2023-03-02 00:03:35 -0700

    setup.sh should be able to setup home as the dotfiles repo at the very least

--- setup.sh
+++ setup.sh
@@ -1,30 +1,22 @@
-
-(
+#place dotfiles repo in home
 cd ~
 git init
 git remote add git@github.com:harleydutton/dotfiles.git
-
-
-)
-
-
-
-
-cp .vimrc ~/.vimrc
-cp .inputrc ~/.inputrc
-cp .zshrc ~/.zshrc
-
-#determine package manager
-PM="something or other"
-
-
-#cli=vim zsh git python3 ssh java
-#some kind of clipboard manager
-#apps=discord obsidian chrome bitwarden
-#silverblue= fuzzel waybar sway
+git pull origin master --allow-unrelated-histories
+rm -rf ./dotfiles
 
 #install commentary for vim
 mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/commentary.git
 vim -u NONE -c "helptags commentary/doc" -c q
+
+
+#determine package manager
+# PM="something or other"
+
+#cli=vim zsh git python3 ssh java
+#some kind of clipboard manager
+#apps=discord obsidian chrome bitwarden
+#silverblue= fuzzel waybar sway
+
