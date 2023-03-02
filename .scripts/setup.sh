f3939451d6e9c66861cc52ed1afb2d5922ea00ed
harleydutton <harleydutton@gmail.com>
2023-03-02 01:18:38 -0700

    starting on the install scripts

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,3 +1,5 @@
+# cd ~; git clone git@github.com:harleydutton/dotfiles.git; ./dotfiles/.scripts/setup.sh
+
 #place dotfiles repo in home
 cd ~
 git init
@@ -7,17 +9,14 @@ git commit --allow-empty -m "from setup.sh"
 git pull origin master --rebase -X theirs --allow-unrelated-histories
 rm -rf ./dotfiles
 
-#install commentary for vim
-# mkdir -p ~/.vim/pack/tpope/start
-# cd ~/.vim/pack/tpope/start
-# git clone https://tpope.io/vim/commentary.git
-# vim -u NONE -c "helptags commentary/doc" -c q
+#installs
+./.scripts/install-commentary.sh
 
 
 #determine package manager
 # PM="something or other"
 
-#cli=vim zsh git python3 ssh java
+#cli=vim zsh python3 ssh java
 #some kind of clipboard manager
 #apps=discord obsidian chrome bitwarden
 #silverblue= fuzzel waybar sway
