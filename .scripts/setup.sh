37937accc5969133e92c14e83e5bff193cff0e08
harleydutton <harleydutton@gmail.com>
2023-03-02 01:33:03 -0700

    adding upstream tracking settings

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -6,6 +6,7 @@ git init
 git remote add origin git@github.com:harleydutton/dotfiles.git
 echo "*" >> .gitignore
 git commit --allow-empty -m "from setup.sh"
+git branch -u origin/master
 git pull origin master --rebase -X theirs --allow-unrelated-histories
 rm -rf ./dotfiles
 
