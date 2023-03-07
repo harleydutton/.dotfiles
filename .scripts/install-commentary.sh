01d5d7fdb995ae3db0ec04e435d83519c5620657
Dutton <harleydutton@gmail.com>
2023-03-06 18:16:41 -0700

    didn't mean to delete these

---
+++ .scripts/install-commentary.sh
@@ -0,0 +1,5 @@
+#install commentary for vim       
+mkdir -p ~/.vim/pack/tpope/start
+cd ~/.vim/pack/tpope/start                   
+git clone https://tpope.io/vim/commentary.git
+vim -u NONE -c "helptags commentary/doc" -c q
