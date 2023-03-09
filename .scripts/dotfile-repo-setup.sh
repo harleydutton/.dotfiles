5b9a73a5fb1a3ab1058fe1410d6fa3d2f6b823f3
harley <harleydutton@gmail.com>
2023-03-09 15:32:21 -0500

    setting default email

--- .scripts/dotfile-repo-setup.sh
+++ .scripts/dotfile-repo-setup.sh
@@ -2,8 +2,8 @@ cd ~
 git init
 git remote add origin git@github.com:harleydutton/dotfiles.git
 echo "*" >> .gitignore
-git commit --allow-empty -m "from setup.sh"
+git commit --allow-empty -m "from dotfile-repo-setup.sh"
+git fetch
 git branch -u origin/main
 git pull origin main --rebase -X theirs --allow-unrelated-histories
-git fetch
 rm -rf ./dotfiles README.md
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-cd ~; git clone git@github.com:harleydutton/dotfiles.git; ./dotfiles/.scripts/git-ssh-setup.sh; read; ./dotfiles/.scripts/dotfiles-repo-setup.sh;
+cd ~; git clone git@github.com:harleydutton/dotfiles.git; ./dotfiles/.scripts/git-ssh-setup.sh; read; ./dotfiles/.scripts/dotfile-repo-setup.sh;
