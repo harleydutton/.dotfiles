639538b88e060fb2f9999eb3507fe6ccaa4f833c
harleydutton <harleydutton@gmail.com>
2023-03-09 14:45:33 -0500

    Update setup.sh

--- .scripts/setup.sh
+++ .scripts/setup.sh
@@ -1,4 +1,14 @@
-# cd ~; git clone git@github.com:harleydutton/dotfiles.git; ./dotfiles/.scripts/setup.sh
+# cd ~; git clone https://github.com/harleydutton/dotfiles.git; ./dotfiles/.scripts/setup.sh
+
+#variables
+EMAIL="harleydutton@gmail.com"
+
+#git and ssh
+git config --global init.defaultBranch main
+git config --global user.email $EMAIL
+ssh-keygen -t ed25519 -C $EMAIL
+eval `ssh-agent`
+ssh-add ~/.ssh/id_ed25519
 
 #place dotfiles repo in home
 cd ~
