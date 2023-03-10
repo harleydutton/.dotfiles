7a61cc668123dcc29e9fb0daffd48877f0122e2c
Dutton <harleydutton@gmail.com>
2023-03-10 13:29:53 -0700

    patched in main

--- .scripts/dotfile-repo-setup.sh
+++ .scripts/dotfile-repo-setup.sh
@@ -1,12 +1,9 @@
-# cd ~; git clone git@github.com:harleydutton/dotfiles.git; ./dotfiles/.scripts/setup.sh
-
-#place dotfiles repo in home
 cd ~
 git init
 git remote add origin git@github.com:harleydutton/dotfiles.git
 echo "*" >> .gitignore
-git commit --allow-empty -m "from setup.sh"
+git commit --allow-empty -m "from dotfile-repo-setup.sh"
+git fetch
 git branch -u origin/main
 git pull origin main --rebase -X theirs --allow-unrelated-histories
-rm -rf ./dotfiles
-git fetch
+rm -rf ./dotfiles README.md
---
+++ README.md
@@ -0,0 +1,1 @@
+cd ~; git clone git@github.com:harleydutton/dotfiles.git; ./dotfiles/.scripts/git-ssh-setup.sh; read; ./dotfiles/.scripts/dotfile-repo-setup.sh;
