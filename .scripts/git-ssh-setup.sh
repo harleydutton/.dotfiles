d391e07a82bb756f41105d996351e3d99272c4e4
Dutton <harleydutton@gmail.com>
2023-03-10 13:40:36 -0700

    fixed the key name

--- .scripts/git-ssh-setup.sh
+++ .scripts/git-ssh-setup.sh
@@ -1,7 +1,7 @@
-EMAIL="harleydutton@gmail.com"
+EMAIL="harley.dutton@charter.com"
 git config --global init.defaultBranch main
 git config --global user.email $EMAIL
 ssh-keygen -t ed25519 -C $EMAIL
 eval `ssh-agent`
 ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed.25519.pub
+cat ~/.ssh/id_ed25519.pub
