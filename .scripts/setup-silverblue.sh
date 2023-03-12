4c1a3ab721ee1dd6cc0b5c1ce6ab1bcb2c5e47f5
Harley Dutton <harleydutton@gmail.com>
2023-03-12 19:31:36 -0400

    continuing the work

--- .scripts/setup-silverblue.sh
+++ .scripts/setup-silverblue.sh
@@ -1,2 +1,3 @@
+rpm-ostree install vim zsh keychain sway fuzzel waybar
 flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install md.obsidian.Obsidian flathub com.discordapp.Discord com.google.Chrome
