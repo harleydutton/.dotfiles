39058a35ef4b993a6757418e3d984e3dc7b8e6d8
Harley Dutton <harleydutton@gmail.com>
2023-03-12 23:14:16 -0400

    fixed a typo in zsh plugins script

--- .scripts/install-zsh-plugins.sh
+++ .scripts/install-zsh-plugins.sh
@@ -1,7 +1,8 @@
-ZSH_PLUGINS=~/.zhs_plugins
+ZSH_PLUGINS=~/.zsh_plugins
 rm -rf $ZSH_PLUGINS
 mkdir -p $ZSH_PLUGINS
 cd $ZSH_PLUGINS
+pwd
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
 git clone https://github.com/agkozak/zsh-z.git
