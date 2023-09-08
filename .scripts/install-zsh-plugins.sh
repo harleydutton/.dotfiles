1f05c2d6cdfbcd206d30b440d42ebadfaac13e29
Harley Dutton <harleydutton@gmail.com>
2023-09-07 22:49:33 -0600

    reset and test

--- .scripts/install-zsh-plugins.sh
+++
@@ -1,7 +0,0 @@
-echo "zsh plugin dir: "$ZSH_PLUGINS
-rm -rf $ZSH_PLUGINS
-mkdir -p $ZSH_PLUGINS
-cd $ZSH_PLUGINS
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
-git clone https://github.com/agkozak/zsh-z.git
--- .zprofile
+++
@@ -1,2 +0,0 @@
-#KEYCHAIN
-eval `keychain -q -k others --eval ~/.ssh/id_ed25519`
