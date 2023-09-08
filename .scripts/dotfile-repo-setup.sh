b5a3bcb1b5886e1223e720570248e826b0d9858b
Harley Dutton <harleydutton@gmail.com>
2023-09-07 22:33:49 -0600

    testing

--- .scripts/dotfile-repo-setup.sh
+++
@@ -1,9 +0,0 @@
-cd ~
-git init
-git remote add origin git@github.com:harleydutton/dotfiles.git
-echo "*" >> .gitignore
-git commit --allow-empty -m "from dotfile-repo-setup.sh"
-git fetch
-git branch -u origin/main
-git pull origin main --rebase -X theirs --allow-unrelated-histories
-rm -rf ./dotfiles README.md
