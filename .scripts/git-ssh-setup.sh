5851bc4eedcd2f0c244c605df8ca2649417ae533
Harley Dutton <harleydutton@gmail.com>
2024-04-05 19:08:37 -0600

    decoupled ssh and git stuff

--- .scripts/git-ssh-setup.sh
+++
@@ -1,7 +0,0 @@
-EMAIL="harleydutton@gmail.com"
-git config --global init.defaultBranch main
-git config --global user.email $EMAIL
-ssh-keygen -t ed25519 -C $EMAIL
-eval `ssh-agent`
-ssh-add ~/.ssh/id_ed25519
-cat ~/.ssh/id_ed25519.pub
