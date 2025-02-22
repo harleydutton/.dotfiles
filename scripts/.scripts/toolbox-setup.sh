07052d8ca46ba8a7c58891431e60616c3319c15b
Harley Dutton <harleydutton@gmail.com>
2025-02-22 01:01:06 -0500

    putting vscode back in the toolbox

--- scripts/.scripts/toolbox-setup.sh
+++ scripts/.scripts/toolbox-setup.sh
@@ -1,1 +1,4 @@
-sudo dnf install -y zsh foot vim
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install -y code zsh foot vim
