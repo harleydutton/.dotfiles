4f9ff28ee60689fa36dbfeae7740ec581f2ab0e5
harleydutton <harleydutton@gmail.com>
2026-04-29 02:24:17 -0600

    working music pipeline

--- .local/bin/music-sync.sh
+++ .local/bin/music-sync.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+
+# DOWNLOAD NEW MUSIC
+#yt-dlp -x "https://www.youtube.com/watch?v=qlrpeYdm9Ec&list=PL6RC7EWwJgkLLmnYqiTEksxWjVuKKvrOI"                                                                     
+
 # UPLOAD NEW MUSIC
 mkdir -p ~/Downloads/new-music
 if [ -n "$(ls ~/Downloads/new-music)" ]; then
     rclone copy ~/Downloads/new-music gdrive:Music
     if [ $? -eq 0 ]; then
         rm -f ~/Downloads/new-music/*
-        rclone dedupe --dedupe-mode newest gdrive:Music
     else
         echo "Aborting: Failed to upload all files!"
         exit 1
@@ -15,14 +18,5 @@ fi
 # TWO WAY SYNK DELETE > MODIFY
 rclone sync gdrive:Music ~/Music --ignore-existing
 rclone sync ~/Music gdrive:Music --ignore-existing
-rclone bisync gdrive:Music ~/Music
 
 
-# CREATE SNAPSHOT
-#   LATEST_SNAPSHOT=$(rclone lsd gdrive:Music-Snapshots/ 2>/dev/null | awk '{print $NF}' | sort | tail -1)
-#   THIRTY_DAYS_AGO=$(date -d "30 days ago" +%Y-%m-%d)
-
-#   if [[ -z "$LATEST_SNAPSHOT" || "$LATEST_SNAPSHOT" < "$THIRTY_DAYS_AGO" ]]; then
-#       echo "Taking snapshot..."
-#       rclone copy gdrive:Music gdrive:Music-Snapshots/$(date +%Y-%m-%d)
-#   fi
--- .local/bin/sys-tb-create.sh
+++ .local/bin/sys-tb-create.sh
@@ -1,7 +1,7 @@
 # default system toolbox -- included:
 # btop for monitoring
 # screenshot tools (grim, slurp, wl-copy)
-# music management tools (yt-dlp, rclone)
-#toolbox create sys
-#toolbox enter sys
-sudo dnf install -y grim slurp wl-copy btop rclone
+# music management tools (yt-dlp, ffprobe, ffmpeg, libshaderc, spirv-tools python3-pip) (rclone)
+sudo dnf install -y grim slurp wl-copy btop rclone ffmpeg ffprobe python3-pip spirv-tools libshaderc
+python3 -m pip install -U --pre "yt-dlp[default]"
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-  $(rpm -E %fedora).noarch.rpm 
