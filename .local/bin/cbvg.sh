237993e6cf3d8bff9ca9b679b52911771ede97ec
harleydutton <harleydutton@gmail.com>
2026-05-01 17:17:01 -0600

    dependencies

--- .local/bin/cbvg.sh
+++
@@ -1,4 +0,0 @@
-cd ~/Workspace/CardBoardVideoGame/app
-source venv/bin/activate
-sudo firewall-cmd --add-port=8080/tcp
-python app.py
--- .local/bin/sys-tb-create.sh
+++ .local/bin/sys-tb-create.sh
@@ -1,7 +1,12 @@
 # default system toolbox -- included:
-# btop for monitoring
-# screenshot tools (grim, slurp, wl-copy)
-# music management tools (yt-dlp, ffprobe, ffmpeg, libshaderc, spirv-tools python3-pip) (rclone)
-sudo dnf install -y grim slurp wl-copy btop rclone ffmpeg ffprobe python3-pip spirv-tools libshaderc
+# monitor.desktop (btop)
+# screenshot.sh (grim, slurp, wl-copy)
+# music download (yt-dlp)
+# dupes.py (opustags)
+# music-sync.sh (rclone)
+# tagger.sh (fpcalc)
+# unsure: ffprobe, ffmpeg, libshaderc, spirv-tools, python3-pip)
+# pip reqs in pipx or similar: numpy, chromaprint
+sudo dnf install -y grim slurp wl-copy btop rclone ffmpeg ffprobe python3-pip spirv-tools libshaderc opustags
 python3 -m pip install -U --pre "yt-dlp[default]"
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-  $(rpm -E %fedora).noarch.rpm 
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm 
