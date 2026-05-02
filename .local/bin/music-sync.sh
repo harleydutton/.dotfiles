87085ebc5c362b9334892097ff0e7a5a38d89468
harleydutton <harleydutton@gmail.com>
2026-05-01 21:39:44 -0600

    more naming conventions

--- .local/bin/music-sync.sh
+++ .local/bin/music-sync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # TWO WAY SYNK DELETE > MODIFY
-rclone sync gdrive:Music ~/Music --ignore-existing
-rclone sync ~/Music gdrive:Music --ignore-existing
+#rclone sync gdrive:Music ~/Music --ignore-existing
+#rclone sync ~/Music gdrive:Music --ignore-existing
+echo "I don't trust this program. Do more testing and flesh it out"
+
 
 
